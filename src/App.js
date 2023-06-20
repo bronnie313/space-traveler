6baf0375825155fc5126e9f46d0dd0432b9329cf
Fezzopro <fezzopro@gmail.com>
2023-06-20 12:42:48 +0200

    Add App component to house all other components as well as the initial component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Navigation/Navigation';
+import NotFound from './components/NotFound/NotFound';
+import Rockets from './components/Rockets/Rockets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Nav />
+      </div>
+      <div className="container">
+        <Routes>
+          <Route index element={<Rockets />} />
+          <Route path="rockets" element={<Rockets />} />
+          {/* <Route path="mission" element={<Missions />} /> */}
+          {/* <Route path="profile" element={<Profile />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 }
