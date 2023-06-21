e46f57f5562458d9223881eaf5f4eef54e89e126
Fezzopro <fezzopro@gmail.com>
2023-06-21 23:34:35 +0200

    Dispatch rocket fetch rocket action and render returned results

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,9 +1,11 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import './rockets.css';
-import rocketsFromStorage from './rocketsLists';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
 const RocketItem = ({ rockets }) => {
   const rocketLists = rockets.map((rocket) => {
-    const id = { ...rocket };
+    const { id } = rocket;
     return (
       <li key={id} className="rocket-list-item">
         <div className="rocket-img-div">
@@ -12,7 +14,7 @@ const RocketItem = ({ rockets }) => {
         <div>
           <h3>{rocket.name}</h3>
           <p>{rocket.description}</p>
-          <button type="button" className="reserve-btn">{rocket.status ? 'Cancel Reservation' : ' Reserve Ticket'}</button>
+          {rocket.reserved ? <button type="button" className="reserve-btn">Cancel Reservation</button> : <button type="button" className="reserve-btn reserved">Reserve Ticket</button>}
         </div>
       </li>
     );
@@ -21,12 +23,20 @@ const RocketItem = ({ rockets }) => {
   return rocketLists;
 };
 
-const Rockets = () => (
-  <div className="rockets">
-    <ul className="rocket-list-items">
-      <RocketItem rockets={rocketsFromStorage()} />
-    </ul>
-  </div>
-);
+const Rockets = () => {
+  const rocketLists = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+  return (
+    <div className="rockets">
+      <ul className="rocket-list-items">
+        {rocketLists.error !== '' ? <div className="error"><span>{rocketLists.error}</span></div> : null}
+        {rocketLists.isLoading ? <div className="loading"><span /></div> : <RocketItem rockets={rocketLists.rocketLists} />}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
