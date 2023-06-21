9343266334ff62be38703b8e5851620743177957
Fezzopro <fezzopro@gmail.com>
2023-06-21 23:36:55 +0200

    Add rocket action and reducers. As well as creating async thunk to fetch API data

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,15 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { ROCKETS_URL } from '../../config/globals';
 
 const initialState = {
-  rockets: [
-    {
-      name: '',
-      description: '',
-      img: '',
-      status: '',
-    },
-  ],
+  rocketLists: [],
+  error: '',
+  message: '',
+  isLoading: true,
 };
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', () => (
+  axios
+    .get(ROCKETS_URL)
+    .then((response) => response.data)
+));
 
 export const rocketsSlice = createSlice({
   name: 'rockets',
@@ -18,6 +21,24 @@ export const rocketsSlice = createSlice({
     createReservation: (state, { payload }) => {
       state.reservations.push(payload.reservation);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const tmpList = [];
+        action.payload.forEach((rocket) => {
+          tmpList.push({ ...rocket, reserved: false });
+        });
+        state.rocketLists = tmpList;
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
