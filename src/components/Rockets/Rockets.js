be7794b294f44c56cbe4d901f0cba95e3e0e5c16
Fezzopro <fezzopro@gmail.com>
2023-06-22 13:51:44 +0200

    Refactor Rocket component and remove the dispatch

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,20 +1,22 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './rockets.css';
-import { fetchRockets } from '../../redux/rockets/rocketsSlice';
-import { RocketItem } from './RocketItem';
+import RocketItem from './RocketItem';
+
+const Rocket = ({ rockets }) => {
+  const rocketLists = rockets.map((rocket) => (
+    <RocketItem rocket={rocket} key={Math.random() * 10000} />
+  ));
+
+  return rocketLists;
+};
 
 const Rockets = () => {
   const rocketLists = useSelector((state) => state.rockets);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, []);
   return (
     <div className="rockets">
       <ul className="rocket-list-items">
         {rocketLists.error !== '' ? <div className="error"><span>{rocketLists.error}</span></div> : null}
-        {rocketLists.isLoading ? <div className="loading"><span /></div> : <RocketItem rockets={rocketLists.rocketLists} />}
+        {rocketLists.isLoading ? <div className="loading"><span /></div> : <Rocket rockets={rocketLists.rocketLists} />}
       </ul>
     </div>
   );
