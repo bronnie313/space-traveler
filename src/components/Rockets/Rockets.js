2a2a355aa83c1a9b6432d67b6acc2d22da31d269
Fezzopro <fezzopro@gmail.com>
2023-06-22 10:56:09 +0200

    Refactor rocket component

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -2,26 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import './rockets.css';
 import { fetchRockets } from '../../redux/rockets/rocketsSlice';
-
-const RocketItem = ({ rockets }) => {
-  const rocketLists = rockets.map((rocket) => {
-    const { id } = rocket;
-    return (
-      <li key={id} className="rocket-list-item">
-        <div className="rocket-img-div">
-          <img src={rocket.flickr_images[0]} alt="Rocket" />
-        </div>
-        <div>
-          <h3>{rocket.name}</h3>
-          <p>{rocket.description}</p>
-          {rocket.reserved ? <button type="button" className="reserve-btn">Cancel Reservation</button> : <button type="button" className="reserve-btn reserved">Reserve Ticket</button>}
-        </div>
-      </li>
-    );
-  });
-
-  return rocketLists;
-};
+import { RocketItem } from './RocketItem';
 
 const Rockets = () => {
   const rocketLists = useSelector((state) => state.rockets);
