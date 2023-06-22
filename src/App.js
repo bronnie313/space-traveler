614e7e374e897018842f158bb678e6b031cd2aef
Fezzopro <fezzopro@gmail.com>
2023-06-22 13:50:35 +0200

    Dispatch the fetchRocket action at the start of the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Nav from './components/Navigation/Navigation';
 import Rockets from './components/Rockets/Rockets';
 import Missions from './components/Missionslist';
 import Profile from './components/Profile';
+import { fetchRockets } from './redux/rockets/rocketsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
   return (
     <div>
       <div>
