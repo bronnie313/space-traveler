2052c976ea0bd76ebd18dbcfb2a5ba054d3b6cce
Fezzopro <fezzopro@gmail.com>
2023-06-24 16:55:18 +0200

    Add Rocket module tests

--- src/components/__tests__/Rocket.test.js
+++ src/components/__tests__/Rocket.test.js
@@ -1,29 +1,16 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { Provider } from 'react-redux';
-// import { store } from '../features/store';
-// import Rockets from '../Rockets/Rockets';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Rockets from '../Rockets/Rockets';
+import store from '../features/store';
 
-// test('renders Rockets component correctly', () => {
-//   const rocketList = [
-//     {
-//       id: 1,
-//       rocket_name: 'Falcon 9',
-//       description: 'A two-stage rocket designed and manufactured by SpaceX.',
-//       image: 'falcon9.jpg',
-//       reserved: false,
-//     },
-//   ];
-
-//   const { container } = render(
-//     <Provider store={store}>
-//       <Rockets rocketList={rocketList} />
-//     </Provider>,
-//   );
-
-//   expect(container.firstChild).toMatchSnapshot();
-// });
-
-test('test', () => {
-  expect(true).toBe(true);
+describe('Rockets Components test suits', () => {
+  test('Should render Rockets component correctly', () => {
+    const container = render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
