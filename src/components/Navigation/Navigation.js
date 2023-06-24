b349e3e5f743201637bc796ed6fb21cff3c7e63a
Fezzopro <fezzopro@gmail.com>
2023-06-24 15:03:57 +0200

    Change navigation active class

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -15,9 +15,15 @@ const Nav = () => (
       </div>
       <div className="nav-link">
         <ul>
-          <li><NavLink exact activeClassName="active" to="rockets">Rockets</NavLink></li>
-          <li><NavLink activeClassName="active" to="Missions">Missions</NavLink></li>
-          <li><NavLink activeClassName="active" to="profile">My Profile</NavLink></li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'none')} to="rockets">Rockets</NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'none')} to="Missions">Missions</NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'none')} to="profile">My Profile</NavLink>
+          </li>
         </ul>
       </div>
     </nav>
